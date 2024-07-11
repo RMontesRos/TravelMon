7bb9b524413db8031077006ba0136ddcab480bd5
Ruth <rmontesros@outlook.es>
2024-07-11 21:09:25 +0200

    TM-11: Home CTA

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import SearchBar from '../components/SearchBar';
 import Features from '../components/Features';
+import CTA from '../components/CTA';
 import '../styles/HomePage.css';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
     <div className='full-content'>
       <SearchBar withImage={true} />
       <Features />
+      <CTA />
     </div>
   );
 };
